74dd41f977d1084433be34ad8e5cb1d813b22e97
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2024-03-25 16:01:51 -0300

    Teclas funcionando

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,36 @@ const playPiano = (nota) => {
 
     audio.play();
 };
+
+const pianoKeyboard = {
+    "Tab": () => {playPiano("1")}, //Dó 4
+    "1": () => {playPiano("2")}, // Dó# 4
+    "q": () => {playPiano("3")}, // Ré 4
+    "2": () => {playPiano("4")}, // Ré# 4
+    "w": () => {playPiano("5")}, // Mi 4
+    "e": () => {playPiano("6")}, // Fá 4
+    "4": () => {playPiano("7")}, // Fá# 4 
+    "r": () => {playPiano("8")}, // Sol 4
+    "5": () => {playPiano("9")}, // Sol# 4
+    "t": () => {playPiano("10")}, // Lá 4
+    "6": () => {playPiano("11")}, // Lá# 4
+    "y": () => {playPiano("12")}, // Si 4
+    "u": () => {playPiano("13")}, // Dó 5
+    "8": () => {playPiano("14")}, // Dó# 5
+    "i": () => {playPiano("15")}, // Ré 5
+    "9": () => {playPiano("16")}, // Ré# 5
+    "o": () => {playPiano("17")}, // Mi 5
+    "p": () => {playPiano("18")}, // Fá 5
+    "-": () => {playPiano("19")}, // Fá# 5
+    "Dead": () => {playPiano("20")}, // Sol 5
+    "=": () => {playPiano("21")}, // Sol# 5
+    "[": () => {playPiano("22")}, // Lá 5
+    "Backspace": () => {playPiano("23")}, // Lá# 5
+    "Enter": () => {playPiano("24")}, // Si 5
+}
+
+document.addEventListener("keydown", (event) => {
+    event.preventDefault()
+
+    pianoKeyboard[event.key]()
+});
