973cce0cd278b3c0de8edce1623cc9a272a255aa
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2024-03-25 11:15:16 -0300

    Piano funcional

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
+const keys = document.querySelectorAll(".keys");
+
+keys.forEach((key) => {
+    key.addEventListener("click", () => {
+        playPiano(key.getAttribute("data-note"));
+    });
+});
+
 const playPiano = (nota) => {
-    const audio = new Audio(`audio/${nota}.mp3`);
+    const audio = new Audio(`audio/${nota}.wav`);
 
     audio.play();
 };
